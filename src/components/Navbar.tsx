59d42ea299defd65a3b9ad4d0c48d4b56e2a48df
Suryanshu Singh <1singhsuryanshu@gmail.com>
2026-03-09 04:46:46 -0400

    Add new font + Worked on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,26 @@
+import { Button } from "@/components/ui/button"
+
 export default function Navbar() {
   return (
-    <div className='fixed top-3 left-3 h-10 w-1/2 bg-white z-50 rounded-lg flex items-center justify-center'>
-      <h1 className="text-pink-300 text-xl font-bold">Bouq.it</h1>
+    <div className="fixed top-3 inset-x-0 h-10 bg-transparent z-50 px-3">
+      <div className="mx-auto flex h-full max-w-6xl items-center justify-between">
+        <h1 className="text-pink-300 text-xl font-medium">Bouq.it</h1>
+
+        <div className="flex items-center gap-4">
+          {/* Add links in here*/}
+          <div className="flex items-center gap-4 bg-pink-300 rounded-full px-4 h-8">
+          <a className="text-white text-sm font-medium" href="/">Home</a>
+          <a className="text-white text-sm font-medium" href="/">About</a>
+          </div>
+          <Button
+            variant="default"
+            size="icon"
+            className="bg-pink-300 text-white w-20 h-8 rounded-full font-bold"
+          >
+            Sign in
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
