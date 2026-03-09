7c32bc2f8133dd7a36d820a11d7a17d3777d666b
Suryanshu Singh <1singhsuryanshu@gmail.com>
2026-03-09 02:21:28 -0400

    feat: initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div className='fixed top-3 left-3 h-10 w-1/2 bg-white z-50 rounded-lg flex items-center justify-center'>
+      <h1 className="text-pink-300 text-xl font-bold">Bouq.it</h1>
+    </div>
+  )
+}
